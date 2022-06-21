3752e964f3d0c0326bdbbe890fc1192c60fb1293
victor <victor3790@gmail.com>
2022-06-21 09:02:46 -0500

    First version revised

--- ph-hotmart.php
+++ ph-hotmart.php
@@ -27,6 +27,8 @@ class Ph_Hotmart
     {
 
         add_action( 'rest_api_init', [ $this, 'set_endpoint' ] );
+        add_action( 'init', [ $this, 'register_custom_order_status' ] );
+        add_filter( 'wc_order_statuses', [ $this, 'add_custom_order_status' ] );
 
     }
 
@@ -60,6 +62,14 @@ class Ph_Hotmart
 
         $JSON_data = file_get_contents('php://input');
         $data = json_decode( $JSON_data, true );
+
+        if( is_null( $data ) ){
+
+            $this->send_error_mail( 'Hotmart did not send any data.' );
+            return;
+
+        }
+
         $email = $data['data']['buyer']['email'];
 
         $customer_id = null;
@@ -91,6 +101,42 @@ class Ph_Hotmart
 
     }
 
+    public function register_custom_order_status()
+    {
+
+        $args = array(
+            'label'                     => 'Hotmart Completed',
+            'public'                    => true,
+            'show_in_admin_status_list' => true, 
+            'show_in_admin_all_list'    => true,
+            'exclude_from_search'       => false,
+            'label_count'               => _n_noop( 'Hotmart Completed <span class="count">(%s)</span>', 'Hotmart Completed <span class="count">(%s)</span>' )
+        );
+        register_post_status( 'wc-hotmart-completed', $args );
+
+    }
+
+    public function add_custom_order_status( $order_statuses )
+    {
+
+        $new_order_statuses = array();
+
+        foreach ($order_statuses as $key => $value) {
+        
+            $new_order_statuses[$key] = $value;
+
+            if( $key === 'wc-completed' ) {
+
+                $new_order_statuses['wc-hotmart-completed'] = 'Completado en Hotmart';
+
+            }
+
+        }
+
+        return $new_order_statuses;
+
+    }
+
     public function check_origin()
     {
 
@@ -114,10 +160,10 @@ class Ph_Hotmart
     private function add_order( $customer_id, $product_id, $price )
     {
 
-        $order_args = array( 'status' => 'completed', 'customer_id' => $customer_id );
+        $order_args = array( 'status' => 'wc-hotmart-completed', 'customer_id' => $customer_id );
 
         $order = wc_create_order( $order_args );
-        $product = get_product( $product_id );
+        $product = wc_get_product( $product_id );
         $order->add_product( $product, 1 );
         $order->set_total($price);
         $order->save();
